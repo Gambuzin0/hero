a57afc251899e2221d625710dffc00ebe0b8a855
Leandro <jose.leandro2002@gmail.com>
2021-11-13 22:42:38 +0000

    Added Walls

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -6,16 +6,31 @@ import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arena {
     private int width,height;
     private Hero hero = new Hero(20,10);
+    private List<Wall> walls;
 
     public Arena(int width, int height){
         this.width = width;
         this.height = height;
+        this.walls = createWalls();
     }
-
+    private List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+        for (int c = 0; c < width; c++) {
+            walls.add(new Wall(c, 0));
+            walls.add(new Wall(c, height - 1));
+        }
+        for (int r = 1; r < height - 1; r++) {
+            walls.add(new Wall(0, r));
+            walls.add(new Wall(width - 1, r));
+        }
+        return walls;
+    }
     public void moveHero(Position position){
         if(canHeroMove(position)){
             hero.setPosition(position);
@@ -23,7 +38,7 @@ public class Arena {
     }
 
     public boolean canHeroMove(Position position){
-        if(position.getY() >= height || position.getY() < 0 || position.getX() >= width || position.getX() < 0)
+        if(position.getY() >= height-1 || position.getY() < 1 || position.getX() >= width -1 || position.getX() < 1)
             return false;
         return true;
     }
@@ -31,6 +46,8 @@ public class Arena {
     public void draw(TextGraphics graphics){
         graphics.setBackgroundColor(TextColor.Factory.fromString("#00008B"));
         graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
+        for (Wall wall : walls)
+            wall.draw(graphics);
         hero.draw(graphics);
     }
 
